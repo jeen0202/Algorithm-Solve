63ecf98d37845020aacfcf083a4232da6d204ada
Sejing <jeen0202@korea.ac.kr>
2021-10-28 16:33:42 +0900

    10/28 List 정리

--- Programmers/2021/10/102801/102801.py
+++ Programmers/2021/10/102801/102801.py
@@ -1,19 +1,27 @@
 import sys
+from collections import Counter
+
 sys.stdin = open(sys.argv[1],'r')
 arr  = [list(map(str,input().split())) for _ in range(2)]
 arr[1] = list(map(int,arr[1]))
 
-def solution(orders,course):
-    print(orders)
+def solution(orders,course):    
+    counter = set()
+    for item in orders:
+        for j in item:
+            counter.add(j)
+    counter= sorted(counter)
+    count = {} 
+    print(counter)
     print("==============")
     answer = []
-    for max in course:
-        for i in orders:
-            for j in orders:
-                if len(i.serach(j)) == max :
-                    print(orders[i].match(orders[j]))
+    for item in counter:
+        print(orders.count(item))
+        #count.update(item=orders.count(item))
+            
+                        
                     
-    return answer   
+    return count 
 
 if __name__ == '__main__':    
     print(solution(arr[0],arr[1]))
