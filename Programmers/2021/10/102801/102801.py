349d0b14b3bdb92f82ffe185b971a07855289b7a
Sejing <jeen0202@korea.ac.kr>
2021-10-28 11:29:32 +0900

    10/28 solving....

--- Programmers/2021/10/102801/102801.py
+++ Programmers/2021/10/102801/102801.py
@@ -4,7 +4,16 @@ arr  = [list(map(str,input().split())) for _ in range(2)]
 arr[1] = list(map(int,arr[1]))
 
 def solution(orders,course):
-    return orders, course
+    print(orders)
+    print("==============")
+    answer = []
+    for max in course:
+        for i in orders:
+            for j in orders:
+                if len(i.serach(j)) == max :
+                    print(orders[i].match(orders[j]))
+                    
+    return answer   
 
 if __name__ == '__main__':    
     print(solution(arr[0],arr[1]))
