3ab7e7967a4bec5a45af56e7ecbeb516aa93b3f6
Sejing <jeen0202@korea.ac.kr>
2021-10-29 12:09:46 +0900

    10/29 연속 for 문제해결시도

--- Programmers/2021/10/102801/102801.py
+++ Programmers/2021/10/102801/102801.py
@@ -11,18 +11,38 @@ def solution(orders,course):
         for j in item:
             counter.add(j)
     counter= sorted(counter)
-    count = {} 
-    print(counter)
-    print("==============")
+    count = {}
+    count_pair = {}
     answer = []
-    for item in counter:
-        print(orders.count(item))
-        #count.update(item=orders.count(item))
+    for item in counter:        
+        value = {}
+        values = 0
+        for string in orders:                
+                values += string.count(item)
+                if item in string:
+                    for c in string:
+                        if item != c:                            
+                            if c in value:
+                                value[c] += string.count(c)  
+                            else : value[c]=1
+        count[item] = values
+        value = sorted(value.items(),key=lambda x:x[1], reverse=True)
+        count_pair[item]=value
+        #print(value)
+    # for item in count_pair:
+    #     print(item,count_pair[item])
+    count= sorted(count.items(),key=lambda x: x[1],reverse=True)
+    #print(count)
+    for index in course:
+        temp = ""
+        temp += count[0][0]
+        #for j in range(index-1):
             
                         
                     
-    return count 
+    return answer
 
 if __name__ == '__main__':    
     print(solution(arr[0],arr[1]))
+    #solution(arr[0],arr[1])
 
