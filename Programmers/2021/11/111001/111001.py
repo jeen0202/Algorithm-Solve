922217ede732413a281d0e4f62e95630925c2405
Sejin Kim <jeen0202@korea.ac.kr>
2021-11-11 17:09:27 +0900

    11/11 Greedy?

--- Programmers/2021/11/111001/111001.py
+++ Programmers/2021/11/111001/111001.py
@@ -5,25 +5,18 @@ arr  = [list(map(str,input().split())) for _ in range(1)]
 
 def solution(name):
     answer = 0
-    if len(name) == name.count('A'):
-        return 0
-    if len(name) == 1:
-        return ord(name)-65
-    if name.count('A') >0:
-        firstA = name.find('A')        
-        for item in re.finditer(r"A+",name):
-            print(item.group())
-            print(item.span())
-        
-    else:
-        answer += len(name)-1
+    min_move = len(name)-1
     
-    
-    
-    name = list(map(ord,name))
-    #print(name)
-    for item in name:
-        answer += min([item-65,91-item])
+    for index,item in enumerate(name):
+        print([ord(item)-65,91-ord(item)])        
+        answer += min(ord(item)-65,91-ord(item))
+        next = index+1
+        while next <len(name) and name[next] == 'A':
+            next+=1
+            
+        min_move = min(min_move,index+index +len(name)-next)
+        print(min_move)
+    answer += min_move
     return answer
 
 if __name__ == '__main__':    
