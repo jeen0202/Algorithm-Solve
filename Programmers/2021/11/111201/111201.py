7e96ac060c06f81c1eb193909c918098d4103aa1
Sejin Kim <jeen0202@korea.ac.kr>
2021-11-13 11:27:27 +0900

    11/13 길찾기(w.bfs) 1차 성공

--- Programmers/2021/11/111201/111201.py
+++ Programmers/2021/11/111201/111201.py
@@ -3,30 +3,38 @@ queue = deque()
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def bfs(x,y,board,visited):
-    queue = deque([x,y,0])
-    visited[x][y] = True
+def bfs(maps,visited):
+    queue = deque([(0,0,1)])
+    visited[0][0] = 1
     while queue:
-        cur = queue.popleft()
-        if cur[0] ==n-1 and cur[1] == m-1:
-            print(cur[2])
-            return
-        for i in range(4):
+        cur = queue.popleft() 
+        #print(cur)
+        if cur[0] ==n and cur[1] == m:
+            return cur[2]
+        for i in range(4):                        
             xx= dx[i] + cur[0]
             yy= dy[i] + cur[1]
-            if 0<=xx<n and 0<=yy<m and not visited[xx][yy] and board[xx][yy] == 1:
-                visited[xx][yy] = True
-                queue.append((xx,yy,cur[2]+1))
+            
+            #print(xx,yy,visited[xx][yy],maps[xx][yy])            
+            if 0<=xx<=n and 0<=yy<=m and visited[xx][yy] ==0 and maps[xx][yy] ==1:                                 
+                visited[xx][yy] = 1
+                queue.append([xx,yy,cur[2]+1])
+                
 def solution(maps):
     global n,m
     n,m = len(maps)-1, len(maps[0])-1
-    visited = [[0]*m] for _ in range(0,n)]
-    print(visited)
-    n,m= len(maps)-1, len(maps[0])-1
+    visited = [[0]*len(maps[0]) for _ in range(len(maps))]
     if maps[n-1][m] == 0 and maps[n][m-1] ==0:
         return -1
-
+    return bfs(maps,visited)
+    
 
 if __name__ == '__main__':    
-    print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]))
+    print(solution([
+[1,0,1,1,1],
+[1,0,1,0,1],
+[1,0,1,1,1],
+[1,1,1,0,1],
+[0,0,0,0,1]
+]))
 
