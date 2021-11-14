ec4baaf1b7f58399a8985ee1ae4e915f557a1ca4
Sejin Kim <jeen0202@korea.ac.kr>
2021-11-14 19:11:00 +0900

    11/14 BFS

--- Programmers/2021/11/111201/111201.py
+++ Programmers/2021/11/111201/111201.py
@@ -10,6 +10,8 @@ def bfs(maps,visited):
         cur = queue.popleft() 
         #print(cur)
         if cur[0] ==n and cur[1] == m:
+           # for line in visited:
+               # print(line)
             return cur[2]
         for i in range(4):                        
             xx= dx[i] + cur[0]
@@ -19,7 +21,7 @@ def bfs(maps,visited):
             if 0<=xx<=n and 0<=yy<=m and visited[xx][yy] ==0 and maps[xx][yy] ==1:                                 
                 visited[xx][yy] = 1
                 queue.append([xx,yy,cur[2]+1])
-                
+    return -1           
 def solution(maps):
     global n,m
     n,m = len(maps)-1, len(maps[0])-1
