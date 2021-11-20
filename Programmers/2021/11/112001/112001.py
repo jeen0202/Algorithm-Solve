f4855f02c83b0d987e902248a48a7e5fed80f081
Sejin Kim <jeen0202@korea.ac.kr>
2021-11-20 21:31:10 +0900

    11/20 후보키 시작

---
+++ Programmers/2021/11/112001/112001.py
@@ -0,0 +1,11 @@
+from itertools import 
+
+def solution(relation):
+    answer = 0
+    colsize, rowsize = len(relation[0]),len(relation)   
+    
+    return answer
+
+if __name__ == '__main__':    
+    print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+
