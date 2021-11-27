747d01123484cb6862dfe01d2c02228baf7f6236
Sejin Kim <jeen0202@korea.ac.kr>
2021-11-27 16:26:28 +0900

    11/27 단순화 성공

--- Programmers/2021/11/112601/112601.py
+++ Programmers/2021/11/112601/112601.py
@@ -3,19 +3,26 @@ from collections import Counter
 
 def solution(clothes):   
     dict = {}
-    comb = []
-   
+    answer = 1
     val,key = list(zip(*clothes))
     for i in range(len(val)):
         if not key[i] in dict.keys():
             dict[key[i]] = val[i]            
         else:
-            temp = {dict.get(key[i]),val[i]}
+            if type(dict.get(key[i])) != set:                
+                temp = {dict.get(key[i]),val[i]}
+            else :                                                
+                temp = dict.get(key[i])
+                temp.add(val[i])                
             dict[key[i]] = temp
-    for i in range(len(dict)+1):
-        comb.extend(combinations(dict.values(),i))
-    return comb
+    for value in dict.values():                
+        if type(value) == set:            
+            answer *= (len(value)+1)            
+        else:
+            answer *=2
+    return answer-1
 
 if __name__ == '__main__':    
+    
     print(solution([["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]))
-
+    print(solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]))
