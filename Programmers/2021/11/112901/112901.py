5a4279f4c04aa178b0b9c6d27f0e89e693ec4821
Sejing <jeen0202@korea.ac.kr>
2021-11-29 13:55:27 +0900

    11/19 해결

--- Programmers/2021/11/112901/112901.py
+++ Programmers/2021/11/112901/112901.py
@@ -4,15 +4,20 @@ def solution(citations):
     # for index,item in enumerate(citations):
     #         if index+1 >= item:
     #             return item
-    temp = sorted(sorted(set(citations),key = lambda x : citations.index(x)),reverse=True)    
-    print(citations)
+    temp = sorted(sorted(set(citations),key = lambda x : citations.index(x)),reverse=True)   
     count = 0 
-    for item in temp:
-        count += citations.count(item)
-        if count >= item:            
-            return item
+    print(temp)
+    if temp[0] == 0:
+        return 0    
+    for index in range(temp[0],0,-1):             
+        count += citations.count(index)       
+        if count >= index:
+            return index
             
 
 if __name__ == '__main__':    
-    print(solution([3, 0, 6, 1, 5]))
+    print(solution([3,0,6,1,3,5]))
+    print(solution([0,0,0,0,1]))
+    print(solution([12, 11, 10, 9, 8, 1]))
+    print(solution([0,0,0]))
 
