0f4e5a8ce88bd5d1cfaf7017d69f3a87b42fb8fe
Sejing <jeen0202@korea.ac.kr>
2021-11-30 16:46:03 +0900

    11/30 시간 조정(...ing)

--- Programmers/2021/11/113002/113002.py
+++ Programmers/2021/11/113002/113002.py
@@ -1,17 +1,38 @@
 from itertools import combinations
-
-def solution(number,k):
-    answer = 0
-    temp = list(combinations(number,len(number)-k))
-    
-    for item in temp:
-        str = "".join(item)
-        if int(str) > answer:
-            answer = int(str)        
-    return f"{answer}"
-
+import time
+def solution(number,k):    
+    #start = time.time()
+    answer = ['0']
+    cnt = len(number)-k
+    print(cnt)
+    number = list(number)
+    while number:
+        i = number.pop(0)
+        top = len(answer)
+        if len(answer) < cnt:
+            answer.append(i)            
+        else :       
+            while len(answer) and int(answer[-1])-int(i) <0:
+                answer.pop()
+            if top > len(answer) or len(answer)<len(number)-k:
+                answer.append(i)
+       
+           
+            
+    #print(time.time()-start)         
+    return "".join(answer)
 if __name__ == '__main__':    
     print(solution("1924",2))
-    #print(solution("1231234",3))
-    #print(solution("4177252841",4))
+    print(solution("1231234",3))
+    print(solution("4177252841",4))
 
+    # 시간초과 2   
+    # temp = list(map(int,map("".join,combinations(number,len(number)-k))))
+    # f"{max(temp)}"
+    # 시간초과 3
+    # temp = list(map(int,map("".join,combinations(number,len(number)-k))))
+    # temp.sort(reverse=True)
+    # return f"{temp[0]}"
+    # 시간초과 3
+
+    
