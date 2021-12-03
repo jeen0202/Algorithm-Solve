c22b39187944311ad5b432bfe2041a1795651d6e
Sejin Kim <jeen0202@korea.ac.kr>
2021-12-03 19:11:45 +0900

    12/03 test 성공(17.6)

--- Programmers/2021/12/120201/120201.py
+++ Programmers/2021/12/120201/120201.py
@@ -1,7 +1,18 @@
-def solution(k,dugeons):
+def solution(k,dungeons):
     answer = 1
-    a_list = []
-    
+    dungeons.sort(key=lambda x : x[0],reverse=True)
+    while True:
+        flag = answer
+        next = [9,k]
+        for dungeon in dungeons:
+            if dungeon[0] <= k and dungeon[1] < next[1]:
+                next = [dungeons.index(dungeon),dungeon[1]]
+        if next[0]<=8 and next[1] != k:            
+            dungeons.pop(next[0])
+            k-=next[1] 
+            answer +=1
+        else:
+            break   
     return answer        
 
 if __name__ == '__main__':    
