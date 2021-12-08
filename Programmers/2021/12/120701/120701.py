dd8d485ab6275da54b27cbd171de7ad05b325eec
Sejin Kim <jeen0202@korea.ac.kr>
2021-12-08 23:22:40 +0900

    첫번째 제거 알고리즘 완성

--- Programmers/2021/12/120701/120701.py
+++ Programmers/2021/12/120701/120701.py
@@ -1,23 +1,26 @@
 def solution(m,n,board):
     rows,transBoard = "",list()
-    
+    # 원활한 제거를 위한 list 행렬 변환
     for i in range(n):
         for j in range(m-1,-1,-1):
             rows+=board[j][i]
         transBoard.append(rows)        
         rows=""
     print(transBoard)
+    # 제거 위치 확인을 위한 알고리즘
     match = []
     for i in range(n-1):
         for j in range(m-1):
             if transBoard[i][j]==transBoard[i][j+1] and transBoard[i][j:j+2]==transBoard[i+1][j:j+2]:
                 match.append((i,j))
-                match.append((i,j+1))
+                #match.append((i,j+1))
                 match.append((i+1,j))
-                match.append((i+1,j+1))
+                #match.append((i+1,j+1))
                 #print(f'{i} {j}')
-    match_clean = sorted(list(set(match)))
-    print(match_clean)
+    match = sorted(list(set(match)),reverse=True)
+    for l in match:
+        transBoard[l[0]]= transBoard[l[0]][:l[1]]+transBoard[l[0]][l[1]+2:]
+    print(transBoard)
                 
 
 if __name__ == '__main__':    
