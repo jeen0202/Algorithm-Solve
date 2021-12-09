f89501ffb6c5ffb2c63c9632b517a58e8e7d64fc
Sejin Kim <jeen0202@korea.ac.kr>
2021-12-09 20:07:25 +0900

    1차 결과출력(미완성)

--- Programmers/2021/12/120701/120701.py
+++ Programmers/2021/12/120701/120701.py
@@ -1,5 +1,4 @@
-def solution(m,n,board):
-    answer=0
+def solution(m,n,board):    
     rows,transBoard = "",list()
     # 원활한 제거를 위한 list 행렬 변환
     for i in range(n):
@@ -25,10 +24,12 @@ def solution(m,n,board):
         for l in match:
             transBoard[l[0]]= transBoard[l[0]][:l[1]]+transBoard[l[0]][l[1]+2:]
         print(transBoard)
-        
-    return answer
+    temp = 0
+    for l in transBoard:
+        temp+=len(l) 
+    return (m*n)-temp
 
 if __name__ == '__main__':    
-    #print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+    print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
     print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
 
