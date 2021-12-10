bee74f924893bd880ec9f68005483120c2e5619d
Sejing_Mac <jeen0202@korea.ac.kr>
2021-12-10 11:13:44 +0900

    12/10 문제해결

--- Programmers/2021/12/120701/120701.py
+++ Programmers/2021/12/120701/120701.py
@@ -4,9 +4,9 @@ def solution(m,n,board):
     for i in range(n):
         for j in range(m-1,-1,-1):
             rows+=board[j][i]
-        transBoard.append(rows)        
+        transBoard.append(list(rows))        
         rows=""
-    print(transBoard)
+    #print(transBoard)
     # 제거 위치 확인을 위한 알고리즘
     while True:
         match = []
@@ -14,23 +14,27 @@ def solution(m,n,board):
             for j in range(len(transBoard[i])-1):
                 if transBoard[i][j]==transBoard[i][j+1] and transBoard[i][j:j+2]==transBoard[i+1][j:j+2]:
                     match.append((i,j))
-                    #match.append((i,j+1))
+                    match.append((i,j+1))
                     match.append((i+1,j))
-                    #match.append((i+1,j+1))
+                    match.append((i+1,j+1))
                     #print(f'{i} {j}')
         match = sorted(list(set(match)),reverse=True)
-        print(match)
+        #print(match)
         if match == []:
             break
         for l in match:
-            transBoard[l[0]]= transBoard[l[0]][:l[1]]+transBoard[l[0]][l[1]+2:]
-        print(transBoard)
+                transBoard[l[0]][l[1]] = ''
+        result = []
+        for line in transBoard:
+            result.append(list(''.join(line)))        
+        transBoard=result
+
     temp = 0
     for l in transBoard:
         temp+=len(l) 
     return (m*n)-temp
 
 if __name__ == '__main__':    
-    print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+    #print(solution(4,5,["CCBDE", "AAADE", "AAABF", "CCBBF"]))
     print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]))
 
