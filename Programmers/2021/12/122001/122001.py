d2619ee35906bef9188cc3abd65acf3566698f9a
Sejing_Mac <jeen0202@korea.ac.kr>
2021-12-21 13:32:11 +0900

    12/21 비트 연산(시간초과2)

--- Programmers/2021/12/122001/122001.py
+++ Programmers/2021/12/122001/122001.py
@@ -1,22 +1,17 @@
 def solution(numbers):
     answer = []
     for item in numbers: 
-        temp = item
-        array = []
+        temp = item        
         while True:
             temp +=1
-            count = len(bin(temp))-len(bin(item))
-            if count >2: break
-            #print(bin(temp)[2:])           
+            count = len(bin(temp))-len(bin(item))                   
             for i in range(1,len(bin(item))-1):
                 if bin(item)[-i] != bin(temp)[-i]:
                     count+=1
             if count >2: 
                 continue
-            array.append(temp)
-        #print(answer)
-        #print(min(answer))
-        answer.append(min(array))
+            answer.append(temp)
+            break
     return answer
 if __name__ == '__main__':    
     print(solution([2,7]))
