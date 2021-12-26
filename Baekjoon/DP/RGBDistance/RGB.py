1d1dd9c81a6180c75331022b5f85da1115bf7574
Sejing_Mac <jeen0202@korea.ac.kr>
2021-12-26 18:18:15 +0900

    12/26 RGB(실패02)

--- Baekjoon/DP/RGBDistance/RGB.py
+++ Baekjoon/DP/RGBDistance/RGB.py
@@ -3,17 +3,31 @@ sys.stdin = open("Baekjoon/DP/RGBDistance/input.txt","r")
 
 def solution():
     N = int(input())
-    answer = []    
+    data,answer = [],[]    
     for _ in range(N):
-        data = list(map(int,input().split()))
+        data.append(list(map(int,input().split())))
+        temp = data[-1]
         if not len(answer):
-            answer.append((data.index(min(data)),min(data)))
+            answer.append((temp.index(min(temp)),min(temp)))
+        elif len(answer)==1:
+            temp[answer[-1][0]]= 1001
+            answer.append((temp.index(min(temp)),min(temp)))
         else:
-        # elif len(answer)==1:
-            data[answer[-1][0]] = 1001
-            answer.append((data.index(min(data)),min(data)))
-        #print(data)
+            temp[answer[-1][0]] = 1001
+            #temp[answer[-2][0]] = 1001
+            answer.pop()
+            mini = 2000
+            result = [-1,-1]
+            for i in range(3):
+                for j in range(3):
+                    if data[-1][i]+data[-2][j]< mini:
+                        mini = data[-1][i]+data[-2][j]
+                        print(mini)
+                        result = [i,j]
+            answer.append((result[0],temp[result[0]]))
+            answer.append((result[1],temp[result[1]]))
+    #print(data)
+    print(answer)
     print(sum(item[1] for item in answer))
-
 if __name__ == "__main__":
     solution()
