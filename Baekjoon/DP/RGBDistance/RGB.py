a11c26e092f0eaf03f6ae7b920204ffb5aaa9aad
Sejing_Mac <jeen0202@korea.ac.kr>
2021-12-27 16:56:43 +0900

    12/27 RGB-예제 성공(실패)

--- Baekjoon/DP/RGBDistance/RGB.py
+++ Baekjoon/DP/RGBDistance/RGB.py
@@ -10,24 +10,25 @@ def solution():
         if not len(answer):
             answer.append((temp.index(min(temp)),min(temp)))
         elif len(answer)==1:
-            temp[answer[-1][0]]= 1001
-            answer.append((temp.index(min(temp)),min(temp)))
+            #temp[answer[-1][0]]= 1001
+            answer.append((temp.index(min(temp[:answer[-1][0]]+temp[answer[-1][0]+1:])),min(temp)))
         else:
-            temp[answer[-1][0]] = 1001
+            #temp[answer[-1][0]] = 1001
             #temp[answer[-2][0]] = 1001
             answer.pop()
             mini = 2000
             result = [-1,-1]
             for i in range(3):
                 for j in range(3):
-                    if data[-1][i]+data[-2][j]< mini:
-                        mini = data[-1][i]+data[-2][j]
-                        print(mini)
+                    if i != answer[-1][0] and i!=j and data[-2][i]+data[-1][j]< mini:
+                        mini = data[-2][i]+data[-1][j]
                         result = [i,j]
-            answer.append((result[0],temp[result[0]]))
+                        #print(result)
+            #print(result)
+            answer.append((result[0],data[-2][result[0]]))
             answer.append((result[1],temp[result[1]]))
     #print(data)
-    print(answer)
+    #print(answer)
     print(sum(item[1] for item in answer))
 if __name__ == "__main__":
     solution()
