825aa3cab79ab81851a3de7e7445223eb0af8766
Sejing_Mac <jeen0202@korea.ac.kr>
2021-12-28 21:01:22 +0900

    12/28 RGB(DP적용)

--- Baekjoon/DP/RGBDistance/RGB.py
+++ Baekjoon/DP/RGBDistance/RGB.py
@@ -4,31 +4,16 @@ sys.stdin = open("Baekjoon/DP/RGBDistance/input.txt","r")
 def solution():
     N = int(input())
     data,answer = [],[]    
-    for _ in range(N):
+    for i in range(N):
         data.append(list(map(int,input().split())))
         temp = data[-1]
         if not len(answer):
-            answer.append((temp.index(min(temp)),min(temp)))
-        elif len(answer)==1:
-            #temp[answer[-1][0]]= 1001
-            answer.append((temp.index(min(temp[:answer[-1][0]]+temp[answer[-1][0]+1:])),min(temp)))
+            answer.append(temp)
         else:
-            #temp[answer[-1][0]] = 1001
-            #temp[answer[-2][0]] = 1001
-            answer.pop()
-            mini = 2000
-            result = [-1,-1]
-            for i in range(3):
-                for j in range(3):
-                    if i != answer[-1][0] and i!=j and data[-2][i]+data[-1][j]< mini:
-                        mini = data[-2][i]+data[-1][j]
-                        result = [i,j]
-                        #print(result)
-            #print(result)
-            answer.append((result[0],data[-2][result[0]]))
-            answer.append((result[1],temp[result[1]]))
-    #print(data)
-    #print(answer)
-    print(sum(item[1] for item in answer))
+            answer.append(temp)
+            answer[i][0] = min(answer[i-1][1], answer[i-1][2])+temp[0]
+            answer[i][1] = min(answer[i-1][0], answer[i-1][2])+temp[1]
+            answer[i][2] = min(answer[i-1][0], answer[i-1][1])+temp[2]
+    print(min(answer[N-1][0],answer[N-1][1],answer[N-1][2]))
 if __name__ == "__main__":
     solution()
