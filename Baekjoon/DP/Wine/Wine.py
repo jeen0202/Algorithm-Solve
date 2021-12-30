e93c02c5cf40073350eb5ae4a815be7e362292ae
Sejin Kim <jeen0202@korea.ac.kr>
2021-12-30 17:13:04 +0900

    12/30 Wine(DP) BestCase 참고

--- Baekjoon/DP/Wine/Wine.py
+++ Baekjoon/DP/Wine/Wine.py
@@ -3,13 +3,15 @@ sys.stdin = open("Baekjoon/DP/Wine/input.txt","r")
 
 def solution():
     N = int(input())
-    w = []
+    w,answer = [0],[0]
     for _ in range(N):
         w.append(int(input()))
-    answer = sum(w)
-    for i in range(0,N-5,6):
-        answer-=(min(w[i]+w[i+3],w[i+1]+w[i+4],w[i+2]+w[i+5]))   
-    print(answer)
+    answer.append(w[1])
+    if N>1:
+        answer.append(w[1]+w[2])
+    for i in range(3,N+1):
+        answer.append(max(answer[i-1],answer[i-2]+w[i],answer[i-3]+w[i-1]+w[i]))   
+    print(answer[N])
 
 if __name__ == "__main__":
     solution()
