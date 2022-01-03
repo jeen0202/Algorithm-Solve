13147371e660523bf0c49d1464623b7f84361f6b
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-03 14:42:41 +0900

    01/03 연속합(Dp w.ref)

--- Baekjoon/DP/SeriesSum/SeriesSum.py
+++ Baekjoon/DP/SeriesSum/SeriesSum.py
@@ -2,13 +2,13 @@ import sys
 sys.stdin = open("Baekjoon/DP/SeriesSum/input.txt","r")
 
 def solution():
-    answer = [-1001]
     N = int(input())
     p = list(map(int,input().split()))
-    answer.append(p[0])    
-    for i in range(1,N+1):
-        answer.append(max())
-    
+    dp = [0]* N
+    dp[0] = p[0]
+    for i in range(1,N):
+        dp[i] = (max(p[i],dp[i-1]+p[i]))
+    print(max(dp))
 
 if __name__ == "__main__" :
     solution()
