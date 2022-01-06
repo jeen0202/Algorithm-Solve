f91429cf6fa40dac5e66a9e083ad68b46924b331
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-06 21:22:52 +0900

    01/06 ATM(Greedy완)

--- Baekjoon/Greedy/ATM/ATM.py
+++ Baekjoon/Greedy/ATM/ATM.py
@@ -1,9 +1,15 @@
 import sys
 sys.stdin = open("Baekjoon/Greedy/ATM/input.txt","r")
 
-def solution():
 
-    return
+def solution():
+    N = int(input())
+    time = 0
+    p = list(map(int,input().split()))
+    p.sort()
+    for i in range(N):
+        time += p[i]*(N-i)
+    print(time)
 
 if __name__ == "__main__":
     solution()
