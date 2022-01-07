9e2fa6b97536ed54e5ef3f0e940126815c18e90a
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-07 18:10:28 +0900

    01/07 Coin0(Greedy ~ing)

---
+++ Baekjoon/Greedy/Coin0/Coin0.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open("Baekjoon/Greedy/Coin0/input.txt","r")
+
+
+def solution():
+    N,K = map(int,input().split())
+    A = []
+    coins = 0
+    for _ in range(N):
+        A.append(int(input()))
+    A.reverse()
+    for coin in A:               
+        if K == 0:
+            print(coins)
+            break
+        if coin >K :
+            continue
+        coins += K//coin
+        K = K%coin
+
+if __name__ == "__main__":
+    solution()
