5d7f6eb970d453b5ccbe2a8ceaa6b93878c44e5e
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-08 20:24:18 +0900

    01/08 Greedy 회의실(이중 정렬로 해결)

--- Baekjoon/Greedy/MeetingRoom/MeetingRoom.py
+++ Baekjoon/Greedy/MeetingRoom/MeetingRoom.py
@@ -7,19 +7,20 @@ def solution():
     p,answer= [],[]
     for _ in range(N):
         p.append(list(map(int,input().split())))
-    p.sort(key=lambda x:x[1])
+    p.sort(key=lambda x:(x[1],x[0]))
     for meet in p:
         if len(answer) ==0:
             answer.append(meet)
             continue
         if meet[0] >= answer[-1][1]:
-            answer.append(meet)
+            answer.append(meet) 
+    print(len(answer))
+if __name__ == "__main__":
+    solution()
+
         # flag = True
         # for ans in answer :
         #     if max(ans[0],meet[0])<min(ans[1],meet[1]):
         #         flag=False
         # if flag == True:
-        #     answer.append(meet)    
-    print(len(answer))
-if __name__ == "__main__":
-    solution()
+        #     answer.append(meet)   
