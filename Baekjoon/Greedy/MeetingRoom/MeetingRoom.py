1257c9124e597a2fad445c6bf387551de279addb
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-08 20:13:28 +0900

    01/08 회의실(~ing)

--- Baekjoon/Greedy/MeetingRoom/MeetingRoom.py
+++ Baekjoon/Greedy/MeetingRoom/MeetingRoom.py
@@ -7,14 +7,19 @@ def solution():
     p,answer= [],[]
     for _ in range(N):
         p.append(list(map(int,input().split())))
-    p.sort(key=lambda x: abs(x[0]-x[1]))
+    p.sort(key=lambda x:x[1])
     for meet in p:
-        flag = True
-        for ans in answer :
-            if max(ans[0],meet[0])<min(ans[1],meet[1]):
-                flag=False
-        if flag == True:
-            answer.append(meet)    
-    print(answer)
+        if len(answer) ==0:
+            answer.append(meet)
+            continue
+        if meet[0] >= answer[-1][1]:
+            answer.append(meet)
+        # flag = True
+        # for ans in answer :
+        #     if max(ans[0],meet[0])<min(ans[1],meet[1]):
+        #         flag=False
+        # if flag == True:
+        #     answer.append(meet)    
+    print(len(answer))
 if __name__ == "__main__":
     solution()
