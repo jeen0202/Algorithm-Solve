b9d900c18ac630cf92064b4887ca950e10b38304
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-08 21:49:53 +0900

    01/08 부분최대곱(완)

--- Baekjoon/Practice/Series/Series.py
+++ Baekjoon/Practice/Series/Series.py
@@ -6,11 +6,9 @@ def solution():
     N = int(input())
     p = [] 
     for _ in range(N):
-        p.append(float(input()))        
-    dp = p.copy()      
-    for i in range(len(p)):        
-        for j in range(i+1,len(p)):                  
-            dp[i] = max(dp[i],eval('*'.join(map(str,p[i:j]))))            
-    print(f'{max(dp):.3f}')
+        p.append(float(input())) 
+    for i in range(1,N):
+        p[i] = max(p[i],p[i]*p[i-1])         
+    print(f'{max(p):.3f}')
 if __name__ == "__main__":
     solution()
