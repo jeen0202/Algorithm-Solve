ec490ce21d556a10d6f221733d6296443d3ce77d
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-14 18:48:17 +0900

    01/14 Virus(DFS 완)

--- Baekjoon/Graph/Virus/Virus.py
+++ Baekjoon/Graph/Virus/Virus.py
@@ -8,25 +8,16 @@ for _ in range(M):
     node = list(map(int,input().split(" ")))
     p[node[0]].append(node[1])
     p[node[1]].append(node[0])
-def dfs(start,graph):
-    nvisited, visited = list(),list()
-
-    nvisited.append(start)
-
-    while nvisited:
-        node = nvisited.pop()
+cnt = 0
+def dfs(start,graph,visited = []):
+    global cnt
+    visited.append(start)
+    for node in graph[start]:
         if node not in visited:
-            visited.append(node)
-            nvisited.extend(graph[node])
-    return visited
-def solution():
-    visited = [1]
-    if len(p[1])==0:
-        print(1)
-    else:
-        print(len(dfs(1,p))-1)        
-        
+            dfs(node,graph,visited)
+            cnt +=1
 
-if __name__ == "__main__":
-    solution()
+dfs(1,p)
+print(cnt)       
+
 
