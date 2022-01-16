b7dfd597a89ccdf8f3258f313dc4daac34d2b945
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-16 16:51:14 +0900

    01/16 PT(BFS수정)완

--- Baekjoon/Graph/ParenetsOfTree/ParenetsOfTree.py
+++ Baekjoon/Graph/ParenetsOfTree/ParenetsOfTree.py
@@ -16,7 +16,18 @@ def DFS(start,visited=[],Graph=Graph):
             visited.append(v)
             parents[v] = start
             DFS(v,visited)
-    
-DFS(1)
-for item in parents[2:]:
+
+def BFS(start,Graph=Graph):
+    queue = deque()
+    visited = [0] *(N+1)
+    queue.append(start)
+    while queue:
+        v = queue.popleft()
+        for item in Graph[v]:
+            if visited[item] == 0:
+                visited[item] = v
+                queue.append(item)
+    return visited
+answer = BFS(1)
+for item in answer[2:]:
     print(item)
