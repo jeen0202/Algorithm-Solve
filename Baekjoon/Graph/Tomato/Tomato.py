8fd8ad6914a442443066ee8768cce59b2db01020
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-19 11:29:37 +0900

    01/19 Tomato(~ing)

--- Baekjoon/Graph/Tomato/Tomato.py
+++ Baekjoon/Graph/Tomato/Tomato.py
@@ -4,36 +4,43 @@ M,N = map(int,input().split(" "))
 Graph,visited = [],[]
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-day = 0
+remain = M*N
+day = 1
 for i in range(N):
     Graph.append(list(map(int,input().split(" "))))
+    remain-= Graph[i].count(-1)
 for i in range(N):
     for j in range(M):
         if Graph[i][j] == 1:
+            remain -=1
             visited.append([i,j])
-def doneCheck(visited):
-    check = 0
-    for line in Graph:
-        check += line.count(0)
-    if check == 0:
-        return False
-    return True
-def OneDay(x,y,visited=visited,Graph=Graph):
+def OneDay(x,y,day,visited=visited,Graph=Graph):
+    newT = 0
+    day+=1
     for i in range(4):
         nx,ny = x+dx[i],y+dy[i]
         check = [nx,ny]
         if nx <0 or nx >=N or ny <0 or ny >=M:
             continue
-        if Graph[nx][ny] !=0 and check in visited:
+        if Graph[nx][ny] !=0 or check in visited:
             continue
-        Graph[nx][ny]=1
+        Graph[nx][ny]+=day
+        newT+=1
         visited.append([nx,ny])
-    return -1
-while doneCheck(visited):
-    for item in visited:        
-        OneDay(item[0],item[1])
-    for line in Graph:
-        print(line)
-    print()
+    return newT
+newT = 0
+while True:
+    if remain == 0:
+        print(0)
+        break
+    for x,y in visited:
+        if Graph[x][y] == day:
+            newT += OneDay(x,y,day)
     day+=1
-print(day)
+    if remain == newT:
+        print(day-1)
+        break
+    if newT == 0:
+        print(-1)
+        break
+
