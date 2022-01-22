d4cd410f39e476d805b6cab94d5c912fe9862149
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-22 19:08:38 +0900

    01/22 Population(1회 ~ing)

--- Baekjoon/Graph/Population/Population.py
+++ Baekjoon/Graph/Population/Population.py
@@ -6,28 +6,24 @@ n,l,r = map(int,input().split(" "))
 Graph = []
 for _ in range(n):
     Graph.append(list(map(int,input().split(" "))))
-
-def DFS(graph,root):
-    nv = [[0 for _ in range(n)]for _ in range(n)]
-    nv[root[0]][root[1]] = 1
-    #while nv:
-    x,y = root
-    stack = deque()
-    stack.append((x,y))
+def DFS(root):
+    visited = [[0 for _ in range(n)]for _ in range(n)]
+    stack = [root]
+    nChange = set()
     while stack:
-        s= graph[x][y]
-        cnt = 1
-        sx,sy = stack.pop()
-    for i in range(4):
-        nx,ny = (sum(elem) for elem in zip(dd[i],(sx,sy)))
-        if 0<=nx<n and 0<=ny<n:
-            if l<=abs(graph[nx][ny]-graph[sx][sy]) <=r:
-                stack.append((nx,ny))
-                s+= graph[nx][ny]
-                cnt+=1
-                nv[nx][ny] = cnt
-    for xx,yy in stack:
-        graph[xx][yy] = round(s/cnt)
-    print(nv)
-DFS(Graph,(0,0))
+        x,y = stack.pop()
+        if visited[x][y] == 0:
+            visited[x][y] = 1
+            for i in range(4):
+                nx,ny = x+dd[i][0], y+dd[i][1]
+                if 0<= nx <n and 0<= ny < n:
+                    if l<= abs(Graph[x][y]-Graph[nx][ny]) <=r :
+                        stack.append(((nx,ny)))
+                        nChange.add((nx,ny))
+    output = round(sum(Graph[i[0]][i[1]] for i in nChange)/len(nChange))
+    for (ix,iy) in nChange:
+        Graph[ix][iy] = output
+day = 0
+
+DFS((0,0))
 print(Graph)
