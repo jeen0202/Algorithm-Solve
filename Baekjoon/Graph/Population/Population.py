39606ca99a5860a9891fa2a7fe246c75c88493fa
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-22 20:44:45 +0900

    01/22 Population(제출 실패)

--- Baekjoon/Graph/Population/Population.py
+++ Baekjoon/Graph/Population/Population.py
@@ -1,29 +1,37 @@
 import sys
 sys.stdin = open("Baekjoon/Graph/Population/input.txt","r")
-from collections import deque
 dd = [(-1,0),(1,0),(0,-1),(0,1)]
 n,l,r = map(int,input().split(" "))
 Graph = []
-for _ in range(n):
+for i in range(n):
     Graph.append(list(map(int,input().split(" "))))
-def DFS(root):
+    
+def DFS():
+    ans = 0 
     visited = [[0 for _ in range(n)]for _ in range(n)]
-    stack = [root]
-    nChange = set()
-    while stack:
-        x,y = stack.pop()
-        if visited[x][y] == 0:
-            visited[x][y] = 1
-            for i in range(4):
-                nx,ny = x+dd[i][0], y+dd[i][1]
-                if 0<= nx <n and 0<= ny < n:
-                    if l<= abs(Graph[x][y]-Graph[nx][ny]) <=r :
-                        stack.append(((nx,ny)))
-                        nChange.add((nx,ny))
-    output = round(sum(Graph[i[0]][i[1]] for i in nChange)/len(nChange))
-    for (ix,iy) in nChange:
-        Graph[ix][iy] = output
+    for i in range(n):
+        for j in range(n):
+            stack = [(i,j)]
+            nChange = set()
+            while stack:
+                x,y = stack.pop()
+                if visited[x][y] == 0:
+                    visited[x][y] = 1
+                    for k in range(4):
+                        nx,ny = x+dd[k][0], y+dd[k][1]
+                        if 0<= nx <n and 0<= ny < n:
+                            if l<= abs(Graph[x][y]-Graph[nx][ny]) <=r :
+                                stack.append(((nx,ny)))
+                                nChange.add((nx,ny))
+            if len(nChange) >0:
+                output = round(sum(Graph[i[0]][i[1]] for i in nChange)/len(nChange))
+                for (ix,iy) in nChange:
+                    Graph[ix][iy] = output
+                ans = max(ans,len(nChange))
+    return ans
 day = 0
-
-DFS((0,0))
-print(Graph)
+nc = DFS()
+while nc>0:
+    day +=1
+    nc = DFS()
+print(day)
