58b7eddfb958650dcdb8ac7117b17735d655715d
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-27 22:01:27 +0900

    01/27 ABCDE(~ing)W

--- Baekjoon/Graph/ABCDE/ABCDE.py
+++ Baekjoon/Graph/ABCDE/ABCDE.py
@@ -1,28 +1,29 @@
 import sys
 sys.stdin = open("Baekjoon/Graph/ABCDE/input.txt","r")
-from collections import defaultdict,deque
+from collections import defaultdict
 n,m = map(int,input().split(" "))
 Graph = defaultdict(list)
 for _ in range(m):
     a,b = map(int,input().split(" "))
     Graph[a].append(b)
     Graph[b].append(a)
-print(Graph)
-for i in range(n):
-    visited = []
+visited = []
+def DFS(next,visited):
     q = list()
-    cnt = 0
-    q.append((i,1))
-    visited.append(i)
+    now,cnt = next
+    q.append((now,cnt))
+    visited.append(now)
     while(q):
         a,cnt = q.pop()
         for b in Graph[a]:
             if b not in visited:
-                if cnt+1 >= 5:
+                if cnt+1 == 5:
                     print(1)
                     exit()
-                visited.append(b)
-                q.append((b,cnt+1))
+                DFS((b,cnt+1),visited)
+for i in range(n):
+    visited = []
+    DFS((i,1),visited)
 print(0)
 exit()
     
