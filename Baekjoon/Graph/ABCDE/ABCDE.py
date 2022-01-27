389d88df868486306e3d53da7ff7abd8605a312a
Sejin Kim <jeen0202@korea.ac.kr>
2022-01-27 22:36:35 +0900

    01/27 DFS(재귀)SOL

--- Baekjoon/Graph/ABCDE/ABCDE.py
+++ Baekjoon/Graph/ABCDE/ABCDE.py
@@ -1,29 +1,26 @@
 import sys
 sys.stdin = open("Baekjoon/Graph/ABCDE/input.txt","r")
-from collections import defaultdict
 n,m = map(int,input().split(" "))
-Graph = defaultdict(list)
+Graph = [[]for _ in range(n)]
 for _ in range(m):
     a,b = map(int,input().split(" "))
     Graph[a].append(b)
     Graph[b].append(a)
-visited = []
-def DFS(next,visited):
-    q = list()
-    now,cnt = next
-    q.append((now,cnt))
-    visited.append(now)
-    while(q):
-        a,cnt = q.pop()
-        for b in Graph[a]:
-            if b not in visited:
-                if cnt+1 == 5:
-                    print(1)
-                    exit()
-                DFS((b,cnt+1),visited)
+    
+visited = [False for _ in range(n)]
+
+def DFS(now,cnt):
+    visited[now] = True
+    if cnt >= 4:
+        print(1)
+        exit()
+    for next in Graph[now]:
+        if not visited[next]:
+            DFS(next,cnt+1)
+            visited[next] = False
+
 for i in range(n):
-    visited = []
-    DFS((i,1),visited)
+    DFS(i,0)
+    visited[i] = False
 print(0)
 exit()
-    
