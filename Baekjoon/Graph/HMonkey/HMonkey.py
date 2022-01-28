d517ac3a11182ca058a2d2fdba8a25225ee45d37
Sejing_Mac <jeen0202@korea.ac.kr>
2022-01-28 22:16:31 +0900

    01/28 Hmonkey(BFS ~ing)

--- Baekjoon/Graph/HMonkey/HMonkey.py
+++ Baekjoon/Graph/HMonkey/HMonkey.py
@@ -1,33 +1,59 @@
 import sys
-from tkinter import W
 sys.stdin = open("Baekjoon/Graph/HMonkey/input.txt","r")
+from collections import deque
 k = int(input())
 w,h = map(int,input().split(" "))
 dd = [(-1,0),(1,0),(0,-1),(0,1)]
 kk = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)]
 graph = []
-visited = [[False for _ in range(w)]for _ in range(h)]
+visited = [[-1 for _ in range(w)]for _ in range(h)]
 for _ in range(h):
     graph.append(list(map(int,input().split(" "))))
-cnt =0
-def DFS(start,visited,k,cnt):
-    x,y = start
+cnt = []
+q = deque()
+q.append((0,0,k))
+visited[0][0] = 0
+while q:
+    x,y,k = q.popleft()
     if (x,y) == (h-1,w-1):
-        return cnt 
-    visited[x][y] = True
+        cnt.append(visited[x][y])
     for i in range(4):
-        nx,ny = x+dd[i][0], y+dd[i][1]
-        if 0<=nx < h and 0<= ny <w:
-            if visited[nx][ny] == False and graph[nx][ny] == 0:
-                DFS((nx,ny),visited,k,cnt+1)
-                visited[x][y] = False
+        dx,dy = (sum(e) for e in zip(dd[i],(x,y)))
+        if 0<=dx < h and 0<= dy <w:
+            if visited[dx][dy] == -1 and graph[dx][dy] == 0:
+                visited[dx][dy] = visited[x][y] +1
+                q.append((dx,dy,k))
     if k > 0:
         for j in range(8):
             kx,ky = x+kk[j][0],y+kk[j][1]
             if 0<=kx<h and 0<= ky <w:
-                if visited[kx][ky] == False and graph[kx][ky] ==0:
-                    DFS((kx,ky),visited,k-1,cnt+1)
-                    visited[x][y] = False
-    return 0
-sys.setrecursionlimit(10000**2)
-print(DFS((0,0),visited,k,cnt))
+                if visited[kx][ky] == -1 and graph[kx][ky] == 0:
+                    visited[kx][ky] = visited[x][y]+1
+                    q.append((kx,ky,k-1)) 
+
+if len(cnt):
+    print(cnt)
+    exit()
+print(-1)
+
+# def DFS(start,v,k,c):
+#     x,y = start
+#     if x == h-1 and y == w-1:
+#          cnt.append(c)
+#     v[x][y] = True
+#     for i in range(4):
+#         nx,ny = x+dd[i][0], y+dd[i][1]
+#         if 0<=nx < h and 0<= ny <w:
+#             if v[nx][ny] == False and graph[nx][ny] == 0:
+#                 DFS((nx,ny),v,k,c+1)
+#                 v[x][y] = False
+#     if k > 0:
+#         for j in range(8):
+#             kx,ky = x+kk[j][0],y+kk[j][1]
+#             if 0<=kx<h and 0<= ky <w:
+#                 if v[kx][ky] == False and graph[kx][ky] ==0:
+#                     DFS((kx,ky),v,k-1,c+1)
+#                     v[x][y] = False
+                    
+# sys.setrecursionlimit(10000**2)
+# DFS((0,0),visited,k,0)
