39891a4020cd6f0abb20ff9810fbb720a3136f68
Sejing_Mac <jeen0202@korea.ac.kr>
2022-02-08 15:43:55 +0900

    02/08 Omok(BF)완

--- Baekjoon/BruteForce/Omok/Omok.py
+++ Baekjoon/BruteForce/Omok/Omok.py
@@ -1,47 +1,42 @@
 import sys
 sys.stdin = open("Baekjoon/BruteForce/Omok/input.txt","r")
-from collections import deque
-n,m = 19,19
-dd=  [(0,1),(1,0),(1,1)]
+n= 19
+dd=  [(0,1),(1,0),(1,1),(1,-1)]
 p = [list(map(int,input().split(" "))) for _ in range(n)]
-visited = [[0 for _ in range(m)] for _ in range(n)]
-count = [[-1,-1,0],[-1,-1,0]]
-for line in p:
-    print(line)
 
-def count_1(start):
-    sx,sy = start
-    q = deque()
-    q.append((sx,sy))
-    visited[sx][sy] = 1
-    while q:
-        x,y = q.popleft()
-        for dx,dy in dd:
-            nx,ny = x+dx,y+dy
-            if visited[nx][ny] == 0 and p[nx][ny] == 1:
-                    visited[nx][ny]=visited[x][y]+1
-                    q.append((nx,ny))
-                    if visited[nx][ny] == 5:
-                        print(1)
-                        print(sx,sy)
-def count_2(start):
-    sx,sy = start
-    q = deque()
-    q.append((sx,sy))
-    visited[sx][sy] = 1
-    while q:
-        x,y = q.popleft()
-        for dx,dy in dd:
-            nx,ny = x+dx,y+dy
-            if visited[nx][ny] == 0 and p[nx][ny] == 2:
-                    visited[nx][ny]=visited[x][y]+1
-                    q.append((nx,ny))
-                    if visited[nx][ny] == 5:
-                        print(2)
-                        print(sx,sy)
-for i in range(19):
-    for j in range(19):
-        if p[i][j] == 1:
-            count_1((i,j))
-        elif p[i][j] == 2:
-            count_2((i,j))
+def check():
+    for i in range(n):
+        for j in range(n):
+            visited = [[False for _ in range(n)] for _ in range(n)]
+            if p[i][j]>0:
+                visited[i][j] =1
+                res = [(i,j)]
+
+                for k in range(4):
+                    cnt = 1
+                    nx,ny = i+dd[k][0],j+dd[k][1]
+
+                    while 0<=nx<19 and 0<=ny<19 and visited[nx][ny]== False and p[i][j] == p[nx][ny]:
+                        cnt +=1
+                        visited[nx][ny] = True
+                        res.append((nx,ny))
+                        nx += dd[k][0]
+                        ny += dd[k][1]
+                    if cnt == 5:
+                        cx,cy = i-dd[k][0],j-dd[k][1]
+                        if 0<=cx<19 and 0<=cy<19 and p[cx][cy]==p[i][j]:
+                            continue
+                        else:
+                            return p[i][j], res,k
+    return 0,0,0
+
+a,b,d = check()
+if a>0:
+    print(a)
+    if d == 1:
+        print(b[0][0]+1,b[0][1]+1)
+    else :
+        b.sort(key = lambda x:(x[1],-x[0]))
+        print(b[0][0]+1,b[0][1]+1)
+else :
+    print(a)
