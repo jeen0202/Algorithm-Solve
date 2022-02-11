51df5037a59e92eaf97a1cf7bde287e1520fe66a
Sejing_Mac <jeen0202@korea.ac.kr>
2022-02-11 23:31:04 +0900

    02/11 StartLink(BF)Sol

--- Baekjoon/BruteForce/StartLink/StartLink.py
+++ Baekjoon/BruteForce/StartLink/StartLink.py
@@ -3,20 +3,22 @@ sys.stdin = open("Baekjoon/BruteForce/StartLink/input.txt","r")
 from itertools import combinations
 n = int(input())
 p = [list(map(int,input().split(" ")))for _ in range(n)]
-team = []
-for i in range(1,n):
-    team.extend(combinations(range(n),i))
 ans = sys.maxsize
-for a in team:
+for i in combinations(range(n),n//2):
+    team_a = [*i]
+    team_b = list(set(range(n))-set(team_a))
     sa,sb = 0,0
-    for j in range(n):
-        sb += sum(p[j])    
-    if len(a) ==1:
-        sa= 0
-    else :
-        for i,j in combinations(a,2):
-            sa+= p[i][j] + p[j][i]
-    sb-=sa
+    for j,k in combinations(team_a,2):
+        sa+= p[j][k] + p[k][j]
+    
+    for j,k in combinations(team_b,2):
+        sb += p[j][k] + p[k][j]
     cal = abs(sa-sb)
-    ans = min(ans,cal)
+
+    if cal == 0:
+        print(0)
+        exit()
+    elif cal <= ans:
+        ans = cal
+
 print(ans)
