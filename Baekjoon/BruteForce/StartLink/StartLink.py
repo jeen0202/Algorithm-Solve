3cb1365790ad6126dfb4f29cae976abfc38afcbb
Sejing_Mac <jeen0202@korea.ac.kr>
2022-02-12 18:59:07 +0900

    02/12 StartLink(BF)조건 수정완

--- Baekjoon/BruteForce/StartLink/StartLink.py
+++ Baekjoon/BruteForce/StartLink/StartLink.py
@@ -1,24 +1,21 @@
 import sys
 sys.stdin = open("Baekjoon/BruteForce/StartLink/input.txt","r")
+import sys
 from itertools import combinations
+input = sys.stdin.readline
 n = int(input())
-p = [list(map(int,input().split(" ")))for _ in range(n)]
+arr = [i for i in range(n)]
+p = [list(map(int,input().split()))for _ in range(n)]
 ans = sys.maxsize
-for i in combinations(range(n),n//2):
-    team_a = [*i]
-    team_b = list(set(range(n))-set(team_a))
-    sa,sb = 0,0
-    for j,k in combinations(team_a,2):
-        sa+= p[j][k] + p[k][j]
-    
-    for j,k in combinations(team_b,2):
-        sb += p[j][k] + p[k][j]
-    cal = abs(sa-sb)
-
-    if cal == 0:
-        print(0)
-        exit()
-    elif cal <= ans:
-        ans = cal
-
+for i in range(n):
+    for a in combinations(arr,i):
+        sa,sb = 0,0
+        b = list(set(arr) - set(a))
+        for r in combinations(a,2):
+            sa += (p[r[0]][r[1]] + p[r[1]][r[0]])
+        for r in combinations(b,2):
+            sb += (p[r[0]][r[1]] + p[r[1]][r[0]])
+        ans = min(ans,abs(sa-sb))
+        if ans == 0:
+            break
 print(ans)
