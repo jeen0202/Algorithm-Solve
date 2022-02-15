abe917ff19d3d79cd0c0b8a7453e50b39b1b75b6
Sejing_Mac <jeen0202@korea.ac.kr>
2022-02-15 16:54:12 +0900

    02/15 Tetromino(BF w.dfs)Sol

--- Baekjoon/BruteForce/Tetromino/Tetromino.py
+++ Baekjoon/BruteForce/Tetromino/Tetromino.py
@@ -3,34 +3,43 @@ sys.stdin = open("Baekjoon/BruteForce/Tetromino/input.txt","r")
 n,m = map(int,input().split())
 p = [list(map(int,input().split())) for _ in range(n)]
 dd = [(1,0),(-1,0),(0,1),(0,-1)]
-visited = [[0 for _ in range(m)]for _ in range(n)]
+visit = [[False for _ in range(m)]for _ in range(n)]
+max_check = max(map(max,p))
 ans = 0
-def dfs(start,visit,cnt):
+def dfs(start,cnt,total):
     global ans
     x,y = start
-    visit[x][y] = p[x][y]
-    if cnt == 2:
-        temp = p[x][y]
-        check =0
+    if total + max_check*(4-cnt) <= ans:
+        return
+    if cnt ==4:
+        ans = max(ans,total)
+        return
+    else :
         for dx,dy in dd:
             nx,ny = x+dx,y+dy
-            if 0<=nx<n and 0<=ny<m:
-                temp += p[nx][ny]
-                check+=1
-        if check == 4:
-            ans = max(ans,temp)
-    if cnt <4:
-        for dx,dy in dd:
-            nx,ny = x+dx,y+dy
-            if 0<=nx<n and 0<= ny<m:                
-                visit[nx][ny] = visit[x][y]+p[nx][ny]
-                if cnt+1 ==4:
-                    ans = max(ans,visit[nx][ny])
-                else:
-                    dfs((nx,ny),visit,cnt+1)
-
+            if 0<=nx<n and 0<=ny<m and not visit[nx][ny]:                
+                if cnt ==2:
+                    visit[nx][ny] = True
+                    dfs((x,y),cnt+1,total+p[nx][ny])
+                    visit[nx][ny] = False
+                visit[nx][ny] = True
+                dfs((nx,ny),cnt+1,total+p[nx][ny])
+                visit[nx][ny] = False
 
 for i in range(n):
     for j in range(m):
-        dfs((i,j),visited,1)
+        visit[i][j] = True
+        dfs((i,j),1,p[i][j])
+        visit[i][j] = False
 print(ans)
+
+# def makeT(past,now):
+#     global ans
+#     x,y = now
+#     temp = p[x][y]
+#     for dx,dy in dd:
+#         nx,ny = x+dx,y+dy
+#         if 0<=nx<n and 0<=ny<m:
+#             if (nx,ny) != past:
+#                 temp+=p[nx][ny]
+#     ans = max(ans,temp)
