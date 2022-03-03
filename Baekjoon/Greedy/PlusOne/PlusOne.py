d28360ba2fa5d9fd677dc811cacaae07364416b5
Sejing_Mac <jeen0202@korea.ac.kr>
2022-03-03 14:04:53 +0900

    03/03 PlusOne(Greedy)Time

---
+++ Baekjoon/Greedy/PlusOne/PlusOne.py
@@ -0,0 +1,12 @@
+
+import sys
+sys.stdin = open("Baekjoon/Greedy/PlusOne/input.txt","r")
+n = int(input())
+products = [int(input())for _ in range(n)]
+products.sort(reverse=True)
+ans = 0
+for i in range(n):
+    if i %3 == 2:
+        continue
+    ans+=products[i]        
+print(ans)
