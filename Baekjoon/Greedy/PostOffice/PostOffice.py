7bc76607e96ad8c3856473873cd777f22616e45f
Sejing_Mac <jeen0202@korea.ac.kr>
2022-03-12 20:36:21 +0900

    03/12 PostOffice(Greedy)Sol

--- Baekjoon/Greedy/PostOffice/PostOffice.py
+++ Baekjoon/Greedy/PostOffice/PostOffice.py
@@ -6,10 +6,17 @@ towns = []
 for i in range(n):
     towns.append(list(map(int,input().split())))
 towns.sort(key = lambda x : x[0])
-ans = [0]*n
-for i in range(n):
-    for j in range(n):
-        if i == j:
-            continue
-        ans[i]+= abs(towns[i][0]-towns[j][0])*towns[j][1]
-print(ans.index(min(ans))+1)
+midv = round(sum(p for _, p in towns)/2) 
+count = 0
+for i,p in towns:
+    count += p
+    if count >= midv:
+        print(i)
+        break
+# ans = [0]*n
+# for i in range(n):
+#     for j in range(n):
+#         if i == j:
+#             continue
+#         ans[i]+= abs(towns[i][0]-towns[j][0])*towns[j][1]
+# print(ans.index(min(ans))+1)
