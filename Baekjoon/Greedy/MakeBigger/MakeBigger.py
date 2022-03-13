26905a509c22fb7a834570f1cd4be91a043feef7
Sejing_Mac <jeen0202@korea.ac.kr>
2022-03-13 14:01:25 +0900

    03/13 MakeBigger(Greedy)Sol

--- Baekjoon/Greedy/MakeBigger/MakeBigger.py
+++ Baekjoon/Greedy/MakeBigger/MakeBigger.py
@@ -3,9 +3,11 @@ sys.stdin = open("Baekjoon/Greedy/MakeBigger/input.txt","r")
 input = sys.stdin.readline
 n,k = map(int,input().split())
 number = list(input())
-for i in range(k):
-    for j in range(1,n):
-        if number[j] > number[j-1]:
-            del number[j-1]
-            break
-print(int("".join(number)))
+ans = []
+cnt = k
+for i in range(n):
+    while cnt >0 and ans and ans[-1] < number[i]:
+        ans.pop()
+        cnt -=1
+    ans.append(number[i])
+print("".join(ans[:n-k]))
