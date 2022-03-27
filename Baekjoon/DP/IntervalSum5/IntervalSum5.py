9cfc1279f8d9dc8231adecf4ebeaee4f4b12dbfa
Sejin Kim <jeen0202@korea.ac.kr>
2022-03-27 12:26:48 +0900

    03/27 IntervalSum5(DP)Sol

--- Baekjoon/DP/IntervalSum5/IntervalSum5.py
+++ Baekjoon/DP/IntervalSum5/IntervalSum5.py
@@ -3,14 +3,11 @@ sys.stdin = open("Baekjoon/DP/IntervalSum5/input.txt","r")
 input = sys.stdin.readline
 n,m = map(int,input().split())
 matrix = [list(map(int,input().split()))for _ in range(n)]
-
+dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+dp[1][1] = matrix[0][0]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        dp[i][j] = matrix[i-1][j-1]+ dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
 for _ in range(m):
-    dp = [[0 for _ in range(n)] for _ in range(n)]
     x1,y1,x2,y2 = map(int,input().split())
-    dp[x1-1][y1-1] = matrix[x1-1][y1-1]
-    for i in range(x1-1,x2):
-        for j in range(y1,y2):
-                dp[i][j] = dp[i][j-1]+matrix[i][j]
-        if i+1 <n:
-            dp[i+1][y1-1] = matrix[i+1][y1-1]+dp[i][n-1]
-    print(dp[x2-1][y2-1])   
+    print(dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1])
