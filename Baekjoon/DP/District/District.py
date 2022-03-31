8a53660334939fb35568ab7d7f2f8f0c1db42669
Sejing_Mac <jeen0202@korea.ac.kr>
2022-03-31 14:08:14 +0900

    03/31 District(DP)Hint

--- Baekjoon/DP/District/District.py
+++ Baekjoon/DP/District/District.py
@@ -3,18 +3,15 @@ sys.stdin = open("Baekjoon/DP/District/input.txt","r")
 input = sys.stdin.readline
 n,m = map(int,input().split())
 grounds = []
-dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 for _ in range(n):
     grounds.append(list(map(int,input().split())))
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        if j == 1:
-            dp[i][j] = grounds[i-1][j-1]
-        dp[i][j] = dp[i][j-1]+grounds[i-1][j-1]
+for j in range(m):
+    for i in range(1,n):
+        grounds[i][j] += grounds[i-1][j]
 k = int(input())
 for _ in range(k):
-    ans = 0
     x1,y1,x2,y2 = map(int,input().split())
-    for i in range(x1,x2+1):
-        ans += dp[i][y2] - dp[i][y1-1]
+    ans = sum(grounds[x2-1][y1-1:y2])
+    if x1-2>=0:
+        ans -= sum(grounds[x1-2][y1-1:y2])
     print(ans)
