b94c95b98481746dbfa4120c5c9a8b892e0e416f
Sejing_Mac <jeen0202@korea.ac.kr>
2022-04-28 22:32:42 +0900

    04/28 StringSet(DS)완

--- Baekjoon/DataStructure/StringSet/StringSet.java
+++ Baekjoon/DataStructure/StringSet/StringSet.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.HashMap;
 public class StringSet{
     public static void main(String[] agrs) throws IOException{
         System.setIn(new FileInputStream("Baekjoon/DataStructure/StringSet/input.txt"));
@@ -11,15 +11,12 @@ public class StringSet{
         int n = Integer.parseInt(line.split(" ")[0]);
         int m = Integer.parseInt(line.split(" ")[1]);
         int ans = 0;
-        String[] s = new String[n];
-        for(int i = 0 ; i < n ; i++) s[i] = br.readLine();
+        HashMap<String,Boolean> s = new HashMap<>();
+        for(int i = 0 ; i < n ; i++) s.put(br.readLine(),false);
         for(int j = 0 ; j < m ; j++){
             String temp = br.readLine();
-            for(String item : s){
-                if(item.equals(temp)){
-                    ans +=1;
-                    break;
-                }
+            if(s.get(temp) != null){
+                ans++;
             }
         }
         System.out.println(ans);
